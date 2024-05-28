6c82a09e39f013f15e112b668d22ce91a1c3f77d
kmfoda <kmfoda@gmail.com>
2024-05-28 05:41:52 +0000

    Catch MinimumValidImagesError exception separately

---
+++ neurons/exceptions.py
@@ -0,0 +1,30 @@
+from typing import Any, Dict, Optional
+
+from loguru import logger
+from neurons.constants import MINIMUM_COMPUTES_FOR_SUBMIT
+
+
+class ApiError(Exception):
+    status: int = 400
+    message: str = ""
+    code: str = ""
+
+    def get_status(self) -> int:
+        return self.status
+
+    def to_json(self) -> Optional[Dict[str, Any]]:
+        if self.status in [204]:
+            return None
+
+        if len(self.message) < 1:
+            logger.error(f"Please overwrite message for <{self}>")
+
+        return {"error": self.message, "code": self.code}
+
+
+class MinimumValidImagesError(ApiError):
+    status: int = 400
+    message: str = (
+        f"Submitted compute count must be greater than {MINIMUM_COMPUTES_FOR_SUBMIT}"
+    )
+    code: str = "MINIMUM_VALID_IMAGES_ERROR"
