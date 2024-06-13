bb514f89305e687ae831b05fce5e757b8dcc6e07
Emre Bayram <endemre@yahoo.com>
2024-06-13 09:53:21 +0300

    rebase

--- neurons/validator/main.py
+++ neurons/validator/main.py
@@ -4,12 +4,16 @@ import sys
 
 import sentry_sdk
 
+from neurons.update_checker import check_for_updates
+REPO_URL = "TensorAlchemy/TensorAlchemy"
+
 if __name__ == "__main__":
     # Add the base repository to the path so the validator can access it
     file_path = str(pathlib.Path(__file__).parent.parent.parent.resolve())
     if file_path not in sys.path:
         sys.path.append(file_path)
-
+    current_folder = str(pathlib.Path(__file__).parent.resolve())
+    check_for_updates(current_folder, REPO_URL)
     # Import StableValidator after fixing paths
     from neurons.constants import VALIDATOR_SENTRY_DSN
 
