b413aaaf779c8073dd717dc56a4b09e77d9ecd82
Emre Bayram <endemre@yahoo.com>
2024-07-09 00:58:20 +0300

    refactor miner

--- neurons/miners/StableMiner/miner.py
+++ neurons/miners/StableMiner/miner.py
@@ -14,16 +14,17 @@ from neurons.safety import StableDiffusionSafetyChecker
 from transformers import CLIPImageProcessor
 from utils import warm_up
 
+TASK_PIPELINES = {
+    TaskType.TEXT_TO_IMAGE: AutoPipelineForText2Image,
+    TaskType.IMAGE_TO_IMAGE: AutoPipelineForImage2Image,
+}
+
 
 class StableMiner(BaseMiner):
     def __init__(self) -> None:
-        self.t2i_model_custom: Optional[AutoPipelineForText2Image] = None
-        self.t2i_model_alchemy: Optional[AutoPipelineForText2Image] = None
-        self.i2i_model_custom: Optional[AutoPipelineForImage2Image] = None
-        self.i2i_model_alchemy: Optional[AutoPipelineForImage2Image] = None
+        self.model_configs: Dict[ModelType, Dict[TaskType, ModelConfig]] = {}
         self.safety_checker: Optional[StableDiffusionSafetyChecker] = None
         self.processor: Optional[CLIPImageProcessor] = None
-        self.model_configs: Dict[str, Dict[str, ModelConfig]] = {}
 
         super().__init__()
 
@@ -45,16 +46,6 @@ class StableMiner(BaseMiner):
 
     def load_models(self) -> None:
         try:
-            # Text-to-image
-            self.t2i_model_custom = self.load_t2i_model(self.config.miner.custom_model)
-
-            # Image-to-image
-            # self.i2i_model_custom = self.load_i2i_model(self.t2i_model_custom)
-
-            # TODO: Alchemy model
-            # self.t2i_model_alchemy = None
-            # self.i2i_model_alchemy = None
-
             self.safety_checker = StableDiffusionSafetyChecker.from_pretrained(
                 "CompVis/stable-diffusion-safety-checker"
             ).to(self.config.miner.device)
@@ -63,43 +54,6 @@ class StableMiner(BaseMiner):
             self.setup_model_configs()
         except Exception as e:
             logger.error(f"Error loading models: {e}")
-            raise
-
-    def load_t2i_model(self, model_name: str) -> AutoPipelineForText2Image:
-        try:
-            model = AutoPipelineForText2Image.from_pretrained(
-                model_name,
-                torch_dtype=torch.float16,
-                use_safetensors=True,
-                variant="fp16",
-            ).to(self.config.miner.device)
-
-            model.set_progress_bar_config(disable=True)
-            model.scheduler = DPMSolverMultistepScheduler.from_config(
-                model.scheduler.config
-            )
-
-            return model
-        except Exception as e:
-            logger.error(f"Error loading text-to-image model: {e}")
-            raise
-
-    def load_i2i_model(
-        self, t2i_model: AutoPipelineForText2Image
-    ) -> AutoPipelineForImage2Image:
-        try:
-            model = AutoPipelineForImage2Image.from_pipe(t2i_model).to(
-                self.config.miner.device
-            )
-
-            model.set_progress_bar_config(disable=True)
-            model.scheduler = DPMSolverMultistepScheduler.from_config(
-                model.scheduler.config
-            )
-
-            return model
-        except Exception as e:
-            logger.error(f"Error loading image-to-image model: {e}")
             raise
 
     def get_model_config(
@@ -117,52 +71,74 @@ class StableMiner(BaseMiner):
 
         return self.model_configs[model_type][task_type]
 
+    def load_model(self, model_name: str, task_type: TaskType) -> torch.nn.Module:
+        try:
+            model = TASK_PIPELINES.get(task_type).from_pretrained(
+                model_name,
+                torch_dtype=torch.float16,
+                use_safetensors=True,
+                variant="fp16",
+            )
+
+            model.to(self.config.miner.device)
+            model.set_progress_bar_config(disable=True)
+            model.scheduler = DPMSolverMultistepScheduler.from_config(
+                model.scheduler.config
+            )
+
+            return model
+        except Exception as e:
+            logger.error(f"Error loading {TaskType.value} model: {e}")
+            raise
+
     def setup_model_configs(self) -> None:
         self.model_configs = {
-            # TODO: Implement Alchemy Model
-            # ModelType.ALCHEMY: {
-            #     # Text-to-image
-            #     TaskType.TEXT_TO_IMAGE: ModelConfig(
-            #         args=self.t2i_args,
-            #         model=self.t2i_model_alchemy,
-            #     ),
-            #     TaskType.IMAGE_TO_IMAGE: ModelConfig(
-            #         args=self.i2i_args,
-            #         model=self.i2i_model_alchemy,
-            #     ),
-            # },
             ModelType.CUSTOM: {
                 TaskType.TEXT_TO_IMAGE: ModelConfig(
                     args=self.t2i_args,
-                    model=self.t2i_model_custom,
+                    model=self.load_model(
+                        self.config.miner.custom_model, TaskType.TEXT_TO_IMAGE
+                    ),
                 ),
-                # TODO: implement i2i
                 # TaskType.IMAGE_TO_IMAGE: ModelConfig(
                 #     args=self.i2i_args,
-                #     model=self.i2i_model_custom,
+                #     model=self.load_model(self.config.miner.custom_model, TaskType.IMAGE_TO_IMAGE),
                 # ),
             },
+            # ModelType.ALCHEMY: {
+            #     TaskType.TEXT_TO_IMAGE: ModelConfig(
+            #         args=self.t2i_args,
+            #         model=self.load_model(self.config.miner.alchemy_model, TaskType.TEXT_TO_IMAGE),
+            #     ),
+            #     TaskType.IMAGE_TO_IMAGE: ModelConfig(
+            #         args=self.i2i_args,
+            #         model=self.load_model(self.config.miner.alchemy_model, TaskType.IMAGE_TO_IMAGE),
+            #     ),
+            # },
         }
 
     def optimize_models(self) -> None:
-        # TODO: Alchemy model
         return
-
+        # TODO: the code before was only doing this for alchemy and was deactivated
+        # decide if we want to run this all or only for alchemy; for now leaving as deactive
         if self.config.miner.optimize:
             try:
-                self.t2i_model_alchemy.unet = torch.compile(
-                    self.t2i_model_alchemy.unet,
-                    mode="reduce-overhead",
-                    fullgraph=True,
-                )
+                for model_type, tasks in self.model_configs.items():
+                    for task_type, config in tasks.items():
+                        if config.model:
+                            config.model.unet = torch.compile(
+                                config.model.unet,
+                                mode="reduce-overhead",
+                                fullgraph=True,
+                            )
 
-                # Warm up model
-                logger.info(
-                    ">>> Warming up model with compile... "
-                    "this takes roughly two minutes...",
-                    color="yellow",
-                )
-                warm_up(self.t2i_model_alchemy, self.t2i_args)
+                            # Warm up model
+                            logger.info(
+                                f">>> Warming up {model_type} {task_type} model with compile... "
+                                "this takes roughly two minutes...",
+                                color="yellow",
+                            )
+                            warm_up(config.model, config.args)
             except Exception as e:
                 logger.error(f"Error optimizing models: {e}")
                 raise
