5ab4ab421d79a2d5c9d155189fff2de574d57e7f
Emre Bayram <endemre@yahoo.com>
2024-07-14 00:26:11 +0300

    accept any refiner

--- neurons/miners/StableMiner/schema.py
+++ neurons/miners/StableMiner/schema.py
@@ -21,7 +21,7 @@ class ModelConfig(BaseModel):
     args: Dict[str, Any]
     model: DiffusionPipeline
     model_config = ConfigDict(arbitrary_types_allowed=True)
-    refiner: Optional[torch.nn.Module] = None
+    refiner: Optional[Any] = None
 
 
 class TaskConfig(BaseModel):
