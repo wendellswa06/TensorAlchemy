1469cbbde37bbe90149423c376e48559828706cb
Will Whitty <tavurth@gmail.com>
2024-07-30 18:19:18 +0800

    Update timeout again

--- neurons/validator/validator.py
+++ neurons/validator/validator.py
@@ -571,7 +571,7 @@ class StableValidator:
 
     async def get_image_generation_task(
         self,
-        timeout: int = 3,
+        timeout: int = 60,
     ) -> ImageGenerationTaskModel | None:
         """
         Fetch new image generation task from backend or generate new one
