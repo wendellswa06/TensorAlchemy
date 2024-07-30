f8838cdfaa881c6200eafe47a36ae5ec328e578f
Will <Tavurth@gmail.com>
2024-07-30 15:03:59 +0800

    Add import tests (#98)

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,5 +1,16 @@
 import neurons
 
 
+# NOTE: This file basically just checks that imports work
+
+
 def test_dependencies() -> None:
     pass
+
+
+def test_main_miner() -> None:
+    import neurons.miners.StableMiner.main
+
+
+def test_main_validator() -> None:
+    import neurons.validator.main
