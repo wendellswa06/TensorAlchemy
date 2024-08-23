b7df17afa71656a5a51ed3177aae457dc7a6de37
Will Whitty <tavurth@gmail.com>
2024-08-23 14:04:48 +0700

    Cleanup flow and make logic optional

--- neurons/validator/utils/state.py
+++ neurons/validator/utils/state.py
@@ -1,4 +1,5 @@
 import os
+from typing import List
 from datetime import datetime, timedelta
 
 import torch
@@ -32,75 +33,106 @@ def save_ma_scores(moving_average_scores: torch.Tensor) -> None:
         logger.error(f"Failed to save model with error: {e}")
 
 
-def load_ma_scores() -> torch.Tensor:
+def get_default_ma_scores(use_metagraph_weights: bool = True) -> torch.Tensor:
+    if use_metagraph_weights:
+        logger.info("Loaded MA scores from metagraph weights.")
+        return get_metagraph().weights.to(get_device())
+
+    # NOTE: This was the old format of the way to initialize MA values
+    #       this will be removed in the near future, with preference on the above
+    return torch.zeros(get_metagraph().n).to(get_device())
+
+
+def ensure_cache_valid(file_path: str, hours: int = 4) -> None:
+    # Get file's last modified time
+    last_modified = os.path.getmtime(file_path)
+    last_modified_datetime = datetime.fromtimestamp(last_modified)
+
+    # Get current time
+    current_time = datetime.now()
+
+    # Check if current time is more than 2 hours ahead of last modified time
+    if current_time - last_modified_datetime > timedelta(hours=hours):
+        raise ValueError("State file is more than 2 hours old")
+
+
+def load_ma_scores_from_path(config_path: str) -> torch.Tensor:
     """Load hotkeys and moving average scores from filesystem."""
-    logger.info("Loading previously saved validator state...")
+    file_path = f"{config_path}/model.torch"
+    logger.info(
+        "Attempting to load previously saved "
+        + f"validator state from {config_path}..."
+    )
 
-    moving_average_scores: torch.Tensor = get_metagraph().I.to(get_device())
-    logger.info("Loaded MA scores from incentives.")
+    moving_average_scores: torch.Tensor = get_default_ma_scores()
 
-    file_path = f"{get_config().alchemy.full_path}/model.torch"
+    # NOTE: Uncomment this to enable sync from weights
+    # ensure_cache_valid(file_path, hours=4)
 
-    try:
-        # Check if file exists
-        if not os.path.exists(file_path):
-            raise FileNotFoundError(f"State file not found at {file_path}")
+    # Check if file exists
+    if not os.path.exists(file_path):
+        raise FileNotFoundError(f"State file not found at {file_path}")
 
-        # Get file's last modified time
-        last_modified = os.path.getmtime(file_path)
-        last_modified_datetime = datetime.fromtimestamp(last_modified)
+    state_dict = torch.load(file_path)
+    neuron_weights = torch.tensor(state_dict["neuron_weights"])
 
-        # Get current time
-        current_time = datetime.now()
+    has_nans = torch.isnan(neuron_weights).any()
+    has_infs = torch.isinf(neuron_weights).any()
 
-        # Check if current time is more than 2 hours ahead of last modified time
-        if current_time - last_modified_datetime > timedelta(hours=2):
-            raise ValueError("State file is more than 2 hours old")
+    if has_nans:
+        logger.info(f"Nans found in the model state: {has_nans}")
 
-        state_dict = torch.load(file_path)
-        neuron_weights = torch.tensor(state_dict["neuron_weights"])
+    if has_infs:
+        logger.info(f"Infs found in the model state: {has_infs}")
 
-        has_nans = torch.isnan(neuron_weights).any()
-        has_infs = torch.isinf(neuron_weights).any()
-
-        if has_nans:
-            logger.info(f"Nans found in the model state: {has_nans}")
-
-        if has_infs:
-            logger.info(f"Infs found in the model state: {has_infs}")
-
-        # Check to ensure that the size of the neruon
-        # weights matches the metagraph size.
-        if neuron_weights.shape != (get_metagraph().n,):
-            logger.warning(
-                f"Neuron weights shape {neuron_weights.shape} "
-                + f"does not match metagraph n {get_metagraph().n}"
-                "Populating new moving_averaged_scores IDs with zeros"
-            )
-            moving_average_scores[: len(neuron_weights)] = neuron_weights.to(
-                get_device()
-            )
-            # self.update_hotkeys()
-
-        # Check for nans in saved state dict
-        elif not any([has_nans, has_infs]):
-            moving_average_scores = neuron_weights.to(get_device())
-            logger.info(f"MA scores: {moving_average_scores}")
-            # self.update_hotkeys()
-        else:
-            moving_average_scores = get_metagraph().I
-            logger.info("Loaded MA scores from incentives.")
-
-        # Zero out any negative scores
-        for i, average in enumerate(moving_average_scores):
-            if average < 0:
-                moving_average_scores[i] = 0
-
-        logger.info(
-            f"Loaded model {file_path}",
+    # Check to ensure that the size of the neruon
+    # weights matches the metagraph size.
+    if neuron_weights.shape != (get_metagraph().n,):
+        logger.warning(
+            f"Neuron weights shape {neuron_weights.shape} "
+            + f"does not match metagraph n {get_metagraph().n}"
+            "Populating new moving_averaged_scores IDs with zeros"
         )
+        moving_average_scores[: len(neuron_weights)] = neuron_weights.to(
+            get_device()
+        )
+        # self.update_hotkeys()
 
-    except Exception as e:
-        logger.error(f"Failed to load model with error: {e}")
+    # Check for nans in saved state dict
+    elif not any([has_nans, has_infs]):
+        moving_average_scores = neuron_weights.to(get_device())
+        logger.info(f"MA scores: {moving_average_scores}")
+        # self.update_hotkeys()
+    else:
+        moving_average_scores = get_metagraph().I
+        logger.info("Loaded MA scores from incentives.")
+
+    # Zero out any negative scores
+    for i, average in enumerate(moving_average_scores):
+        if average < 0:
+            moving_average_scores[i] = 0
+
+    logger.info(
+        f"Loaded model {file_path}",
+    )
 
     return moving_average_scores
+
+
+def load_ma_scores() -> torch.Tensor:
+    possible_paths: List[str] = [
+        get_config().alchemy.full_path,
+        get_config().alchemy.full_path.replace(
+            "tensor_alchemy", "image_alchemy"
+        ),
+    ]
+
+    for possible_path in possible_paths:
+        try:
+            return load_ma_scores_from_path(possible_path)
+        except Exception:
+            logger.error(
+                f"Failed to load previous MA scores from {possible_path}"
+            )
+
+    return get_default_ma_scores()
